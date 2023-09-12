856dc481e383826ae3ebb61d9ce6a2cbadcc602c
matteo-gz <1220441774@qq.com>
2023-09-12 20:25:55 +0800

    Update action.js

--- action.js
+++ action.js
@@ -1,23 +1,57 @@
 const fs = require('fs');
-//  
-async function generateReadme() {
+const path = require('path');
 
-  // 读取文件目录
-  const files = fs.readdirSync('.');
+//过滤不需要的文件和目录
+function filterFiles(files) {
+  return files.filter(file => {
+    return file !== '.git' && file !== '.github'; 
+  });
+}
 
-  let markdown = '';
+//递归扫描目录
+function scanDirectory(dir, level = 1) {
+  const files = fs.readdirSync(dir);
 
-  // 生成每个文件/文件夹的Markdown链接
-  for(let file of files) {
-    let link = `- [${file}](./${file})`;
-    markdown += `\n${link}`; 
+  files.forEach(file => {
+    const filepath = path.join(dir, file);
+
+    if(fs.statSync(filepath).isDirectory()) {
+      scanDirectory(filepath, level+1);
+    } else {
+      generateMarkdown(dir, file, level);
+    }
+  });
+}
+
+//生成Markdown
+function generateMarkdown(dir, file, level) {
+
+  //添加headers
+  markdown += `${'#'.repeat(level)} ${path.basename(dir)}\n`;
+
+  //生成链接
+  let link = `- [${file}](${path.join(dir, file)})`;  
+  if(level > 1) {
+    link = `${link} - ${new Date(fs.statSync(path.join(dir, file)).mtime).toLocaleString()}`;
   }
 
-  // 写入README
+  markdown += `${link}\n`;
+}
+
+let markdown = '';
+
+function generateReadme() {
+
+  //过滤文件
+  const files = filterFiles(fs.readdirSync('.'));
+
+  //递归扫描
+  scanDirectory('.');
+
+  //写入README
   fs.writeFileSync('README.md', markdown);
 
   console.log('README generated');
-
 }
 
 generateReadme();
