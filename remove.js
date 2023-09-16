f600e87523cdc1f6efacf91085fa66ad9bfae4d9
m <m>
2023-09-16 13:29:02 +0800

    mv

--- remove.js
+++ remove.js
@@ -1,4 +1,8 @@
 const str = process.argv[2];
 let newStr = str.replace(/ /g, "_");
 newStr = newStr.replace(/:/g, "_");
+newStr = newStr.replace(/\(/g, "_");
+newStr = newStr.replace(/\)/g, "_");
+newStr = newStr.replace(/\//g, "_");
+newStr = newStr.replace(/\'/g, "_");
 console.log(newStr);
